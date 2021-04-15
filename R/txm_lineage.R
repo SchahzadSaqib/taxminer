9bed2767517efc2996380bd17686f16a99583f28
SchahzadSaqib <schahzad.saqib@helsinki.fi>
2021-04-15 11:25:01 +0300

    Added functionality to compile and read a lineage database

--- R/txm_lineage.R
+++ R/txm_lineage.R
@@ -7,10 +7,16 @@
 #' @param taxids (Required) Output table obtained from \link[taxminer]{txm_ecosrc}. Alternatively, a data.frame with
 #'                a single column named "TaxID" and optionally a second column named "AccID".
 #' @param bindtoAcc (Logical) Default TRUE. Bind lineage to accession IDs.
+#' @param Precomp_tbl (Optional) Default NA. Specify the name of the pre-compiled database
+#'                     present within the directory.
+#' @param Precomp_tbl_assign (Optional) Default Dataset + system date. Name of a new compiled database
+#'                            to be assigned
+#' @param savedata (Logical) Default TRUE. Should a compiled database be saved to directory?
 #'
 #' @export
 
-txm_lineage <- function(taxids, bindtoAcc = T) {
+txm_lineage <- function(taxids, bindtoAcc = T, Precomp_tbl = NA,
+                        Precomp_tbl_assign = paste("Dataset_", Sys.Date(), ".rds", sep = ""), savedata = T) {
 
   if (!any(names(taxids) %in% "TaxId")) {
     stop("No column named 'TaxId' - please provide a valid input column")
@@ -20,10 +26,37 @@ txm_lineage <- function(taxids, bindtoAcc = T) {
     stop("No columns named 'AccID' while bindtoAcc is T - please provide a valid accession ID column")
   }
 
-
   ##### Lineage #####
   TaxID_to_src <- taxids %>%
     dplyr::distinct(TaxId)
+
+  ##### Read in pre-compiled database #####
+  if (!is.na(Precomp_tbl)) {
+    print("Reading in dataset and searching for existing lineage")
+    Precomp_tbl_sub <- readRDS(Precomp_tbl) %>%
+      dplyr::right_join(TaxID_to_src, by = "TaxId")
+
+    TaxID_to_src <- TaxID_to_src %>%
+      dplyr::filter(!.data$TaxId %in% Precomp_tbl_sub$TaxId) %>%
+      dplyr::ungroup() %>%
+      dplyr::distinct(.data$TaxId)
+    print(paste(nrow(TaxID_to_src), "of",
+                sum(nrow(Precomp_tbl_sub), nrow(TaxID_to_src)),
+                "will be searched for",
+                sep = " "
+    ))
+  }
+
+
+  ##### Check whether there are any IDs left to search for #####
+  if (nrow(TaxID_to_src) > 0) {
+    if (is.na(Precomp_tbl)) {
+      print(paste("No dataset provided -", nrow(TaxID_to_src),
+                  "accession ids will be searched for",
+                  sep = " "
+      ))
+    }
+
 
   ##### Define split ####
   num_groups <- nrow(TaxID_to_src) / 200
@@ -98,6 +131,30 @@ txm_lineage <- function(taxids, bindtoAcc = T) {
     dplyr::select(.data$TaxId, .data$superkingdom, .data$phylum, .data$class,
                   .data$order, .data$family, .data$genus, .data$species)
 
+  if (savedata) {
+    if (!is.null(lineage)) {
+      if (!is.na(Precomp_tbl)) {
+        if (!is.null(AccIDs_to_src)) {
+          print("Writing new data to Pre-compiled dataset")
+          Precomp_full <- readRDS(Precomp_tbl) %>%
+            dplyr::bind_rows(lineage) %>%
+            saveRDS(file = Precomp_tbl)
+        }
+      } else {
+        print("Creating dataset")
+        saveRDS(lineage, file = Precomp_tbl_assign)
+      }
+    }
+  }
+  if (!is.na(Precomp_tbl)) {
+    lineage <- lineage %>%
+      dplyr::bind_rows(Precomp_tbl_sub)
+  }
+  } else {
+    lineage <- Precomp_tbl_sub
+  }
+
+
   if (bindtoAcc) {
     taxids <- taxids %>%
       dplyr::rename_with(~paste("species"), starts_with("Organism")) %>%
@@ -105,6 +162,6 @@ txm_lineage <- function(taxids, bindtoAcc = T) {
       mutate(TaxId = as.character(TaxId)) %>%
       dplyr::left_join(lineage, by = "TaxId")
   } else {
-    lineage
+    lineage <- lineage
   }
 }
